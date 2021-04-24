a11bfe10b4bcd661d9079b785862545c26dc82f1
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-04-24 17:23:17 -0300

    sequelize

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
   dialect: 'mysql',
-  host: 'localhost'
+  host: 'localhost',
 });
 
 module.exports = sequelize;
