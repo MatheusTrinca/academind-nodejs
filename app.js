54504ace24c7bcd78d41a5b50b8660bd3b0f037a
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-05-24 16:16:04 -0300

    user model

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-//const User = require('./models/user');
+const User = require('./models/user');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -17,27 +17,38 @@ const errorController = require('./controllers/error');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('609d5609da2658001394ac52')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('60abf89cc980f404be2cc7e8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-try {
-  mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-  });
-  app.listen(3000, () => console.log('Connected!'));
-} catch (err) {
-  console.log(err);
-}
+  })
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        User.create({
+          name: 'Matheus',
+          email: 'matheus@test.com',
+          cart: {
+            items: [],
+          },
+        });
+      }
+    });
+    app.listen(3000, () => console.log('Connected!'));
+  })
+  .catch(err => console.log(err));
