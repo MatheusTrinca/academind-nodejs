0fb3da41330870aa432263851c7b0d0e0a349165
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-08-04 17:09:31 -0300

    connect-flash flash()

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,22 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  const message = req.flash('error');
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
     isAuthenticated: false,
+    errorMessage: message.length > 0 ? message[0] : null,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  const message = req.flash('error');
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
     isAuthenticated: false,
+    errorMessage: message.length > 0 ? message[0] : null,
   });
 };
 
@@ -27,8 +31,12 @@ exports.postLogin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email }).select('+password');
-    if (!user) return res.redirect('/login');
+    if (!user) {
+      req.flash('error', 'Invalid email or password.');
+      return res.redirect('/login');
+    }
     if (!(await user.correctPassword(password, user.password))) {
+      req.flash('error', 'Invalid email or password.');
       return res.redirect('/login');
     }
     await setSession(req, res, user, '/');
@@ -43,7 +51,10 @@ exports.postSignup = async (req, res, next) => {
   const confirmPassword = req.body.confirmPassword;
   try {
     const user = await User.findOne({ email: email });
-    if (user) return res.redirect('/signup');
+    if (user) {
+      req.flash('error', 'Email already exists');
+      return res.redirect('/signup');
+    }
     const newUser = new User({
       email: email,
       password: password,
